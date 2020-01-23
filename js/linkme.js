1b31b1bd7fa644cb202fb35ed73511ebca3ad2fd
Rodrigo Emanuel <drigosantos@gmail.com>
2020-01-23 09:40:49 -0300

    Implementado a variavel de inclusao de novos registros

--- js/linkme.js
+++ js/linkme.js
@@ -1,13 +1,11 @@
 function enviar() {
     var emailValue = document.getElementById('emailTxt').value;
     var areaValue = document.getElementById('areaTxt').value;
-
-    console.log(emailValue);
-    console.log(areaValue);
+    var timestamp = new Date().getTime();
 
     // A variavel database vai receber as funções de acesso ao banco de dados
     var database = firebase.database();
-    database.ref('usuario').set({
+    database.ref(timestamp).set({
         email: emailValue,
         mensagem: areaValue
     });
